327971a355ee193d46e33f95d3f3750eb423cf84
Nicolas Paz <nicopazmalizia@gmail.com>
2026-04-06 22:15:13 -0300

    Agregar archivos de proyecto.

---
+++ Programa 0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola profe");
+            Console.WriteLine("Me llamo Nicolas Paz");
+            Console.WriteLine("Curso en la comision 8");
+            Console.WriteLine("Soy un apasionado de la programaicon");
+            Console.WriteLine("Mi objetivo es recibirme este año");
+        }
+    }
+}
